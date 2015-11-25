10fa76641517afff01417575840cdaa2b8129bc2
xdim2006 <xdim2006@yandex.ru>
2015-11-25 16:32:01 +0300

    task done

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCount()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken Token = cts.Token;
+            Assert.AreEqual(55, Program.Count(10, Token));
+            Assert.AreEqual(500500, Program.Count(1000, Token));
+            Assert.AreEqual(500000500000, Program.Count(1000000, Token));
+        }
+        [TestMethod]
+        public void TestStart()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken Token = cts.Token;
+            Assert.AreEqual("userNumber=1000 sumUserNumber=500500", Program.GetCounter("1000", Token));
+            Assert.AreEqual("userNumber=1000000 sumUserNumber=500000500000", Program.GetCounter("1000000", Token));
+        }
+    }
+}
